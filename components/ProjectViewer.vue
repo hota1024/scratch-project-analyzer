22a4829b5470a71b95c2748b9534aa4afc6817c3
hota1024 <hotalog@hota1024.com>
2020-02-04 11:28:53 +0900

    Add ProjectViewer component

---
+++ components/ProjectViewer.vue
@@ -0,0 +1,17 @@
+<template>
+  <div></div>
+</template>
+
+<script lang="ts">
+import { Component, Vue, Prop } from 'nuxt-property-decorator'
+import { Project } from 'project-json'
+
+@Component
+export class ProjectViewer extends Vue {
+  @Prop({ type: Object, required: true }) project!: Project
+}
+
+export default ProjectViewer
+</script>
+
+<style scoped lang="stylus"></style>
