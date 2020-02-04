86957daacd62fd462be84cf6e79df656e7759037
hota1024 <hotalog@hota1024.com>
2020-02-04 11:27:02 +0900

    Add VariableList component

---
+++ components/VariableList.vue
@@ -0,0 +1,53 @@
+<template>
+  <v-card>
+    <v-card-title>
+      <v-icon>{{ titleIcon }}</v-icon>
+      {{ listTitle }}
+      <v-spacer></v-spacer>
+      <v-text-field v-model="search" label="Search"></v-text-field>
+    </v-card-title>
+    <v-data-table
+      :headers="headers"
+      :items="variables"
+      :search="search"
+      :items-per-page="500"
+    ></v-data-table>
+  </v-card>
+</template>
+
+<script lang="ts">
+import { Component, Vue, Prop } from 'nuxt-property-decorator'
+import { Variable } from 'project-json/lib/src'
+
+@Component
+export class VariableList extends Vue {
+  search: string = ''
+
+  headers = [
+    {
+      text: 'Name',
+      align: 'left',
+      value: 'name'
+    },
+    {
+      text: 'Value',
+      align: 'left',
+      value: 'value'
+    }
+  ]
+
+  @Prop({ default: () => '' }) listTitle!: string
+
+  @Prop({ default: () => '' }) titleIcon!: string
+
+  @Prop({ default: () => {} }) variables!: Variable[]
+}
+
+export default VariableList
+</script>
+
+<style lang="stylus">
+.theme--dark.v-data-table tbody tr {
+  transition: all 200ms;
+}
+</style>
