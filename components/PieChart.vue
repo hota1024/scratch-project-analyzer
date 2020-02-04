8ec3cd32c1861ec9c4f379608d7b9443636615fa
hota1024 <hotalog@hota1024.com>
2020-02-04 11:26:16 +0900

    Add PieChart component

---
+++ components/PieChart.vue
@@ -0,0 +1,19 @@
+<script lang="ts">
+import { Component, mixins, Prop } from 'nuxt-property-decorator'
+import { Pie, mixins as vueChartMixins } from 'vue-chartjs'
+import { ChartData, ChartOptions } from 'chart.js'
+
+@Component
+export class PieChart extends mixins(Pie, vueChartMixins.reactiveProp) {
+  @Prop({ default: {} }) chartData!: ChartData
+  @Prop({ default: {} }) options!: ChartOptions
+
+  mounted() {
+    this.renderChart(this.chartData, this.options)
+  }
+}
+
+export default PieChart
+</script>
+
+<style scoped lang="stylus"></style>
